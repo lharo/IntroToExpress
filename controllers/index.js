27caecde5cb908275b7e376aed7fb044ca15ab15
lharo-clip <esteban.haro@payclip.com>
2022-10-21 17:05:06 -0500

    add Swagger

--- controllers/index.js
+++ controllers/index.js
@@ -2,68 +2,96 @@ const FS = require('../firebase');
 const { db } = FS;
 
 const createMovie = async (req, res) => {
-  try{
-      const {body: movie } = req;
-      const moviesDB = db.collection('movies');   
-      const { _path: { sengments } } = await moviesDB.add(movie);
-      const id = sengments[1];
-      res.send({
-          status: 200,
-          id
-      });
-  }catch (error){
-      res.send(error);
-  }
+    try {
+
+        /*  #swagger.parameters['obj'] = {
+                in: 'body',
+                description: 'Structure of movie to be created. This doesn\'t need to have an id yet as the db will be generating it for us',
+                schema: {
+                    $name: 'Avatar',
+                    $author: 'James Cameron',
+                    time: ['20:00', '22:00'],
+                    rating: 5.00
+                }
+        }
+
+        #swagger.responses[200] = {
+                description: 'Movie successfully obtained.',
+                schema: { $ref: '#/definitions/Movie' }
+        }
+
+        #swagger.responses[500] = {
+                description: 'Error.',
+                schema: { $ref: '#/definitions/GenericError' }
+        }
+
+        #swagger.produces = ['application/json']
+        #swagger.consumes = ['application/json']
+
+        */
+
+
+        const { body: movie } = req;
+        const moviesDB = db.collection('movies');
+        const { _path: { sengments } } = await moviesDB.add(movie);
+        const id = sengments[1];
+        res.send({
+            status: 200,
+            id
+        });
+    } catch (error) {
+        res.send(error);
+    }
 };
 
 const getMovie = async (req, res) => {
-  try {
-      const {params : { id }} = req;
-      const moviesDB = db.collection('movies').doc(id);
-      const { _fieldsProto : { time, author, name, rating }} = await moviesDB.get();
-      res.send({
-          status: 200,
-          time: time.stringValue,
-          author: author.stringValue,
-          name: name.stringValue,
-          rating: rating.stringValue
-      })
-  }catch(error){
-      res.send(error);
-  }
+    try {
+        const { params: { id } } = req;
+        const moviesDB = db.collection('movies').doc(id);
+        const { _fieldsProto: { time, author, name, rating } } = await moviesDB.get();
+        res.send({
+            status: 200,
+            time: time.stringValue,
+            author: author.stringValue,
+            name: name.stringValue,
+            rating: rating.stringValue
+        })
+    } catch (error) {
+        res.send(error);
+    }
 }
 
 const deleteMovie = async (req, res) => {
-  try {
-      const { params : { id }} = req;
-      const movieDB = db.collection('movies').doc(id);
-      await movieDB.delete();
-      res.send({
-          status: 200
-      });
-  }catch(error){
-      res.send(error);
-  }
+    try {
+        const { params: { id } } = req;
+        const movieDB = db.collection('movies').doc(id);
+        await movieDB.delete();
+        res.send({
+            status: 200
+        });
+    } catch (error) {
+        res.send(error);
+    }
 }
 
 const updateMovie = async (req, res) => {
-  try{
-      const {body: movie } = req;
-      const { id, time, author, name, rating } = movie;
-      const movieDB = db.collection('movies').doc(id);   
-      const resp = await movieDB.update({
-          name,
-          time,
-          rating,
-          author
-      });
-      res.send({
-          status: 200,
-          id
-      });
-  }catch (error){
-      res.send(error);
-  }
+    try {
+        const { body: movie } = req;
+        const { id, time, author, name, rating } = movie;
+        const movieDB = db.collection('movies').doc(id);
+        const resp = await movieDB.update({
+            name,
+            time,
+            rating,
+            author
+        });
+        res.send({
+            status: 200,
+            id
+        });
+    } catch (error) {
+        res.send(error);
+    }
 }
 
 const getMovies = async (req, res) => {
@@ -73,15 +101,15 @@ const getMovies = async (req, res) => {
         res.send({
             resp
         })
-    }catch(error){
+    } catch (error) {
         res.send(error);
     }
 }
 
 module.exports = {
-  createMovie,
-  getMovie,
-  updateMovie,
-  deleteMovie,
-  getMovies
+    createMovie,
+    getMovie,
+    updateMovie,
+    deleteMovie,
+    getMovies
 }
--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const { router } = require('./routes')
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
+
+
 const app = express();
-const apiPort = process.env.PORT || 8080;
+const apiPort = process.env.PORT || 8088;
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', router);
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
